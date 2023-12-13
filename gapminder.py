91199b102f593d87b733656c21f4d47fbbde4dee
marsh135 <86257034+marsh135@users.noreply.github.com>
2023-12-12 20:00:31 -0500

    fixed gapminder

--- gapminder.py
+++ gapminder.py
@@ -3,7 +3,5 @@
 import plotly.express as px
 
 gapminder_df = px.data.gapminder()
-gapminder2002 = gapminder_df.query('year==2002')
-gapminderUSA = gapminder_df.query('country==United States')
-#print(gapminder2002.head())
-print(gapminderUSA.head())
+
+print(gapminder_df.head())
