1af85e11d051c362d503927895f554dbf9a9007e
marsh135 <86257034+marsh135@users.noreply.github.com>
2024-04-11 11:20:56 -0400

    commit

--- games/PyGame/pgPong.py
+++ games/PyGame/pgPong.py
@@ -29,12 +29,53 @@ class Paddle:
         return self.paddleRect
 
 class Ball:
-    pass
-    def __init__(self):
-        pass
+    def __init__(self, xpos, ypos, speed, radius, color):
+        self.xpos =  xpos
+        self.ypos = ypos
+        self.speed = speed
+        self.radius =  radius
+        self.color =  color
+
+        self.xFac =  1
+        self.yFac = -1
+
+        self.ball =  pygame.draw.circle(
+            screen, self.color, (self.xpos, self.ypos), self.radius)
+        
+        #we will come back to this (maybe)
+        #self.firstPlay = 1
+    
+    def display(self):
+        self.ball = pygame.draw.circle(
+            screen, self.color, (self.xpos, self.ypos), self.radius)
+
+    def update(self):
+        self.xpos += self.speed*self.xFac
+        self.ypos +=  self.speed*self.yFac
+
+        if self.ypos <=0  or self.ypos>= SCREEN_HEIGHT:
+            self.yFac = self.yFac* -1
+    
+    def reset(self):
+        self.xpos =  SCREEN_WIDTH//2
+        self.ypos =  SCREEN_HEIGHT//2
+        self.xFac *=-1
+        self.yFac *=-1
+
+    def hit(self):
+        self.xFac = self.xFac * -1
+
+        #Shorter, but still good
+        #self.xFac *= -1
+
+    def getBall(self):
+        return self.ball
 
 SCREEN_WIDTH =  800
 SCREEN_HEIGHT = 600
+
+clock = pygame.time.Clock()
+FPS = 30
 
 RED = (255,0,0)
 BLUE = (0,0, 255)
@@ -44,8 +85,14 @@ screen =  pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("PONG!")
 
 #create paddles
-paddleRed =  Paddle(20, 0, 3, 100, 10, RED)
-paddleBlue =  Paddle(SCREEN_WIDTH-30, 0, 3, 100, 10, BLUE)
+paddleRed =  Paddle(20, SCREEN_HEIGHT//2-50, 7, 100, 10, RED)
+paddleBlue =  Paddle(SCREEN_WIDTH-30, SCREEN_HEIGHT//2-50, 7, 100, 10, BLUE)
+
+paddleList = [paddleRed, paddleBlue]
+
+#create ball
+ball =  Ball(SCREEN_WIDTH//2, SCREEN_HEIGHT//2, 7, 7, WHITE)
+
 
 
 running =  True
@@ -74,7 +121,7 @@ while running:
             if event.key ==  pygame.K_ESCAPE:
                 running =  False
         if event.type == pygame.KEYUP:
-            if event.key ==  pygame.K_UP:
+            if event.key ==  pygame.K_UP: # and paddle2YFac ==1
                 paddle2YFac = 0
             if event.key ==  pygame.K_DOWN:
                 paddle2YFac = 0
@@ -83,12 +130,20 @@ while running:
             if event.key ==  pygame.K_s:
                 paddle1YFac = 0
             
+    for paddle in paddleList:
+        if pygame.Rect.colliderect(ball.getBall(), paddle.getRect()):
+           ball.hit()
+
     paddleRed.update(paddle1YFac)
     paddleBlue.update(paddle2YFac)
+    ball.update()
 
     paddleRed.display()
     paddleBlue.display()
+    ball.display()
 
     pygame.display.flip()
+    clock.tick(FPS)
+
 
 pygame.quit()
