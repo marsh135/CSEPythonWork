1df8ad08608207b98949bba4c380e8b7536ce7fd
marsh135 <86257034+marsh135@users.noreply.github.com>
2024-04-21 20:12:52 -0400

    Update CaesarCipher.py

--- passwords/CaesarCipher.py
+++ passwords/CaesarCipher.py
@@ -7,16 +7,30 @@ def encrypt(text, shift):
     
     for l in range(len(text)):
         char = text[l]
+        if (char.isupper()):
+            result += chr((ord(char)+shift -65) % 26 +65)
+        else:
+            result += chr((ord(char)+shift -97) % 26 +97)
 
-        result += chr((ord(char)+shift))
+    return result
+
+def decrypt(text, shift):
+    result = ""
+    
+    for l in range(len(text)):
+        char = text[l]
+
+        result += chr((ord(char)-shift))
 
     return result
 
 print("What would you like to encrypt? :")
 
 text =  input()
-shift = 3
+shift = 8
 print("Original Text: " + text)
 
 print("Shift factor: " + str(shift))
 print("Encrypted Message: " + encrypt(text, shift))
+result = encrypt(text, shift)
+print("Decrypted: "  + decrypt(result, shift))
