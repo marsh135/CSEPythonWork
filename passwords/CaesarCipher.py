b72090ec6dbf58457b1072f99184ec3be9c54672
marsh135 <86257034+marsh135@users.noreply.github.com>
2024-11-06 15:17:56 -0500

    commit

--- passwords/CaesarCipher.py
+++ passwords/CaesarCipher.py
@@ -2,17 +2,6 @@
 #take an input string, shift the characters by three letters, 
 #then output the resulting string
 
-def encrypt(text, shift):
-    result = ""
-    
-    for l in range(len(text)):
-        char = text[l]
-        if (char.isupper()):
-            result += chr((ord(char)+shift -65) % 26 +65)
-        else:
-            result += chr((ord(char)+shift -97) % 26 +97)
-
-    return result
 
 def decrypt(text, shift):
     result = ""
@@ -26,13 +15,14 @@ def decrypt(text, shift):
 
     return result
 
-print("What would you like to encrypt? :")
 
-text =  input()
-shift = 8
-print("Original Text: " + text)
+text =  "m[xay_\llvk\cmxw"
+shift = -26
 
-print("Shift factor: " + str(shift))
-print("Encrypted Message: " + encrypt(text, shift))
-result = encrypt(text, shift)
-print("Decrypted: "  + decrypt(result, shift))
+while shift <=27:
+
+    print("Shift factor: " + str(shift), ": Decrypted Message: " + decrypt(text, shift))
+    print()
+    shift+=1
+
+
